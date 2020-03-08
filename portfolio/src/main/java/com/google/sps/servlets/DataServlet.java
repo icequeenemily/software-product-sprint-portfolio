127af7396d53b5f0a84608b893e0aff212abff29
Emily Nesson <emn314@nyu.edu>
2020-03-08 11:18:08 -0400

    Blobstore tutorial: made a BlobstoreUploadUrlServlet.java and FormHandlerServlet.java. When i tried to just map the DataServlet.java to /blobstore-upload-url i was getting a 500 error when i ran devserver. Have doGet(BlobstoreUploadUrlServlet) and doPost(FormHandlerServlet.java) but image isn't actually loading on the page. No errors though. Comments feature does not work now but not sure if this is supposed to override it, which I partly did by changing the form to the one from Blobstore fetch example

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -14,6 +14,8 @@
 
 package com.google.sps.servlets;
 
+import com.google.appengine.api.blobstore.BlobstoreService;
+import com.google.appengine.api.blobstore.BlobstoreServiceFactory;
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
@@ -32,6 +34,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /** Servlet that returns some example content. TODO: modify this file to handle comments data */
+//@WebServlet("/data")
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
 
@@ -43,7 +46,6 @@ private List<String> quotes;
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-
     //Loading Entities
     Query query = new Query("Task").addSort("timestamp", SortDirection.DESCENDING);
 
